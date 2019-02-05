409688f8b7775fbabe28428c3a5934f69f0043f9
Aki <ramanjaneyulu.aki@accenture.com>
2019-02-05 20:53:02 +0530

    MadeChanges

--- CocumberGFTA/src/test/java/Test_Runners/TestRunnerStep.java
+++ CocumberGFTA/src/test/java/Test_Runners/TestRunnerStep.java
@@ -4,11 +4,12 @@ import org.junit.runner.RunWith;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
-
 @RunWith(Cucumber.class)
 @CucumberOptions(features="C:\\Users\\ramanjaneyulu.aki\\Selenium_GFT\\CocumberGFTA\\src\\test\\java"
 	,glue={"Step_Defs"
-	})
+	},plugin = { "pretty", "json:target/cucumber-reports/Cucumber.json" },
+   		 monochrome = true )
 public class TestRunnerStep {
 
 }
+
